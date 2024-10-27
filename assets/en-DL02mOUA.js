b6eb0239f3634ccb8ad1cbbee01239f3a13bdf77
Xabsurd <1565375353@qq.com>
2024-10-27 12:42:58 +0000

    Deploying to gh-pages from @ Xabsurd/absurd-jigsaw-puzzle@6f79b464e7ba306f9789c9b4308692dd4a318473 🚀

---
+++ assets/en-DL02mOUA.js
@@ -0,0 +1,1 @@
+const e="Absurd Jigsaw Puzzle",s="Absurd Jigsaw Puzzle",t="New Game",n="Reset",i="Shuffle",o="Finish",c={title:e,description:s,newGame:t,reset:n,shuffle:i,finish:o};export{c as default,s as description,o as finish,t as newGame,n as reset,i as shuffle,e as title};
